ee961a6a6c3f93a9312ede29ab8e21e5a1500f7e
Lorenzo Salvadori <lorenzo.salvadori.86@gmail.com>
2016-08-11 08:05:55 +0200

    iscale: variables declaration - flag check - input check - output file check MISSING. actual operations

--- 01-Chapter01/06-Listing_1-6-3/06-Listing_1-6-3/iscale.c
+++ 01-Chapter01/06-Listing_1-6-3/06-Listing_1-6-3/iscale.c
@@ -13,18 +13,88 @@ outfile.txt: name of (optional) file to output to
 
 int main(int argc, char* argv[]) 
 {
-	// step through code with argv++ and argc-- to check for '-' sign (a flag) in argv[i][0]
+	// global variables declaration
+	int notes, i;
+	int ismidi = 0;
+	int write_interval = 0;
+	int err = 0;
+	double startval, basefreq, ratio;
+	FILE* fp;
+	double intervals[25];
 
-	// if there is a flag
-		// if the flag is m
-			// set MIDI option
-		
-		//  else if the flag is i
-			// set interval option
+	// check for '-' sign (a flag) in argv[i][0] step through the pointer with argv++ and argc--
 
-		// else
-			// report an incorrect flag error
+	while (argc > 1)
+	{
+		if (argv[1][0] == '-')
+		{
+			if (argv[1][1] == 'm')
+				ismidi = 1;
+			else if (argv[1][1] == 'i')
+				write_interval = 1;
+			else
+			{
+				printf("Error: unrecognized option %s\n.", argv[1]);
+				return 1;
+			}
+
+			argc--;
+			argv++;
+
+		}
+
+		else
+			break;
+	}
+
+	if (argc < 3)
+	{
+		printf("Error: insufficient arguments.\n");
+		printf("Usage: iscale [-m] [-i] N startval [outfile.txt]\n");
+		return 1;
+	}
+
+	// if we got here we should have the correct amount of arguments to process
+	// now on to check if the inputs are whithin range
+
+	notes = atoi(argv[1]);
+	if (notes < 1 || notes > 24)
+	{
+		printf("Error: the number of notes (N) is not in the range 1-24.\n");
+		return 1;
+	}
+
+	startval = atof(argv[2]);
+	if (ismidi)
+	{
+		if (startval < 0.0 || startval > 127.0)
+		{
+			printf("Error: the MIDI starting value (startval) is not in the range 0-127.\n");
+			return 1;
+		}
+	}
+	else
+	{
+		if (startval < 0)
+		{
+			printf("Error: the frequency starting value (startval) is not positive.\n");
+			return 1;
+		}
+	}
+
+	// check for the optional output file
+	fp = NULL;
+	if (argc == 4) 
+	{
+		fp = fopen(argv[3], "w");
+		if (fp == NULL) 
+		{
+			printf("WARNING: unable to create file %s\n", argv[3]);
+			perror(""); //
+		}
+	}
+
+	// if we got here the user has been nice and we can perform the operations
 
 
-	
 }		
